a4c0e6a259c98b434009d644986319834702b6df
rishsol <rishabsolanki09@gmail.com>
2019-10-26 06:24:00 -0400

    Obtaining addresses from Capital One API

--- CapitalOneAPI.py
+++ CapitalOneAPI.py
@@ -1,22 +1,33 @@
 import requests
 import json
-list = []
-import urllib.request
-#raw = urllib.request.urlopen('http://api.reimaginebanking.com/enterprise/customers?key=3bb6dcc2cfc7ec704cfd2678b2816209')
-#json_object = json.load(raw)
+
+addressList = []
+addressStringList = []
 
 url = 'http://api.reimaginebanking.com/enterprise/customers?key=3bb6dcc2cfc7ec704cfd2678b2816209'
 
 json_data = requests.get(url).json()
 
-#print(json_data)
-#for address_data in json_data['results'][0]['address']:
-#	print(address_data)
 
 for i in range(len(json_data['results']) - 1):
-    list.append(json_data['results'][i]['address'])
+    customer_data = json_data['results'][i]
+    addressList.append(customer_data['address'])
+    try:
+        try:
+            if isinstance(int(addressList[i]['street_number']), int):
+                addressStringList.append(addressList[i]['street_number'] + ' ' + addressList[i]['street_name'] + ' ' + addressList[i]['city'] +  ' ' + addressList[i]['state'])
+        except:
+            continue
+    except:
+        continue
+    #addressStringList.append(string)
 
-for i in range(len(list)):
-    list[i] = list[i]
+#for i in range(len(addressStringList)):
+    #try:
+     #   addressStringList[i] = addressStringList[i]
+   # except:
+    #    continue
 
-#print(json_data['results'][5]['address'])
+print(addressStringList)
+print()
+print(addressList[0]['street_number'])
