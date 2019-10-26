30e8baf657b586b18f5e161ab19a77aebddc0073
rishsol <rishabsolanki09@gmail.com>
2019-10-26 02:03:39 -0400

    Iterating over json to obtain only address components

--- CapitalOneAPI.py
+++ CapitalOneAPI.py
@@ -1,9 +1,22 @@
 import requests
 import json
-
+list = []
 import urllib.request
-raw = urllib.request.urlopen('http://api.reimaginebanking.com/enterprise/customers?key=3bb6dcc2cfc7ec704cfd2678b2816209')
-json_object = json.load(raw)
+#raw = urllib.request.urlopen('http://api.reimaginebanking.com/enterprise/customers?key=3bb6dcc2cfc7ec704cfd2678b2816209')
+#json_object = json.load(raw)
 
-for important_data in json_object['results'][0]['address']:
-    print(important_data)
+url = 'http://api.reimaginebanking.com/enterprise/customers?key=3bb6dcc2cfc7ec704cfd2678b2816209'
+
+json_data = requests.get(url).json()
+
+#print(json_data)
+#for address_data in json_data['results'][0]['address']:
+#	print(address_data)
+
+for i in range(len(json_data['results']) - 1):
+    list.append(json_data['results'][i]['address'])
+
+for i in range(len(list)):
+    list[i] = list[i]
+
+#print(json_data['results'][5]['address'])
